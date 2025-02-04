ddf46f39bb82654ed3cf598ba382b5a2f3164d7f
Diana <diana.fg05@hotmail.com>
2025-02-03 21:13:46 -0600

    Lógica de programación 2.

--- logica-programacion/app.js
+++ logica-programacion/app.js
@@ -1,36 +1,28 @@
-//Variables
-let numeroMaximoPosible = 100;
-let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
-let numeroUsuario = 0;
-let intentos = 1;
-//let palabraVeces = 'vez';
-let maximosIntentos = 6;
+let numeroSecreto = generarNumeroSecreto();
+let intentos = 0;
 
+console.log(numeroSecreto);
 
-while (numeroUsuario != numeroSecreto) {
-    numeroUsuario = parseInt(prompt(`Me indicas un número entre 1 y ${numeroMaximoPosible}  por favor:`));
-
-    console.log(typeof(numeroUsuario));
-    if (numeroUsuario == numeroSecreto) {
-        //Acertamos, fue verdadera la condición
-        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces' }`);
-    } else {
-        if (numeroUsuario > numeroSecreto) {
-            alert('El número secreto es menor');
-        } else {
-            alert('El número secreto es mayor');
-        }
-        //Incrementamos el contador cuando no acierta
-        //intentos = intentos + 1;
-        //intentos += 1;
-        intentos++;
-
-        //palabraVeces = 'veces';
-        if (intentos > maximosIntentos) {
-            alert(`Llegaste al número máximo de ${maximosIntentos} intentos`);
-            break;
-        }
-        //La condición no se cumplió
-        //alert('Lo siento, no acertaste el número');
-    }
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
 }
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    if (numeroDeUsuario === numeroSecreto) {
+        console.log('Acertaste el número!');
+    } 
+    return;
+}
+
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;
+
+}
+
+asignarTextoElemento('h1','Juego del número secreto!');
+asignarTextoElemento('p',`Indica un número del 1 al 10`);
